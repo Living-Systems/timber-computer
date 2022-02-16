06f01b7c408becff5621c91c14afe2abc0d8d0cc
Raffael Stüken <r@humans.am>
2022-02-16 23:25:16 +0100

    Add h1

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -1,6 +1,8 @@
 <template>
     <section class="cluster cluster--y-gap">
-        <h2 class="smoke-box--sm p-body">Welcome</h2>
-        <p class="smoke-box--md p-body">Timber Computer helps you understand the potential carbon impact of a future design by configuring a reference building.</p>
+        <h1 class="smoke-box--sm p-body">Welcome</h1>
+        <p class="smoke-box--md p-body">
+            Timber Computer helps you understand the potential carbon impact of a future design by configuring a reference building.
+        </p>
     </section>
 </template>
