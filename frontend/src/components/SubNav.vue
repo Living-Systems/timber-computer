a3e2d6af3c30b090688c04bab4b6bffd2e3efe88
Raffael Stüken <r@humans.am>
2022-02-16 23:23:28 +0100

    Streamline subnav component

--- frontend/src/components/SubNav.vue
+++ frontend/src/components/SubNav.vue
@@ -1,76 +1,95 @@
 <template>
-    <nav class="subnav | text-style-200 uppercase px-body py-200">
-        <ul class="cluster cluster--stretched items-start">
-
-            <li class="subnav__item">
-                <div class="subnav__group-title">
-                    Tragwerk
-                </div>
-                <div class="subnav__item-title">
-                    Decken
-                </div>
+    <nav class="subnav | text-style-200 uppercase">
+        <ol class="cluster cluster--stretched items-start">
+            <li>
+                <a href="#" class="subnav__item is-set">
+                    <div class="subnav__group-title">
+                        Tragwerk
+                    </div>
+                    <div class="subnav__item-title">
+                        Decken
+                    </div>
+                </a>
             </li>
 
-            <li class="subnav__item">
-                <div class="subnav__item-title">
-                    Tragende Wände
-                </div>
+            <li>
+                <a href="#" class="subnav__item is-set">
+                    <div class="subnav__item-title">
+                        Tragende Wände
+                    </div>
+                </a>
             </li>
 
-            <li class="subnav__item">
-                <div class="subnav__item-title">
-                    Dachkonstruktion
-                </div>
+            <li>
+                <a href="#" class="subnav__item is-set">
+                    <div class="subnav__item-title">
+                        Dachkonstruktion
+                    </div>
+                </a>
             </li>
 
-            <li class="subnav__item">
-                <div class="subnav__item-title">
-                    Erschließung
-                </div>
+            <li>
+                <a href="#" class="subnav__item is-current">
+                    <div class="subnav__item-title">
+                        Erschließung
+                    </div>
+                </a>
             </li>
 
-            <li class="subnav__item">
-                <div class="subnav__item-title">
-                    Gründung
-                </div>
+            <li>
+                <a href="#" class="subnav__item">
+                    <div class="subnav__item-title">
+                        Gründung
+                    </div>
+                </a>
             </li>
 
-            <li class="subnav__item">
-                <div class="subnav__group-title">
-                    Hülle
-                </div>
-                <div class="subnav__item-title">
-                    Außenwände
-                </div>
+            <li>
+                <a href="#" class="subnav__item">
+                    <div class="subnav__group-title">
+                        Hülle
+                    </div>
+                    <div class="subnav__item-title">
+                        Außenwände
+                    </div>
+                </a>
             </li>
 
-            <li class="subnav__item">
-                <div class="subnav__item-title">
-                    Verkleidung
-                </div>
+            <li>
+                <a href="#" class="subnav__item">
+                    <div class="subnav__item-title">
+                        Verkleidung
+                    </div>
+                </a>
             </li>
 
-            <li class="subnav__item">
-                <div class="subnav__item-title">
-                    Fenster
-                </div>
+            <li>
+                <a href="#" class="subnav__item">
+                    <div class="subnav__item-title">
+                        Fenster
+                    </div>
+                </a>
             </li>
 
-            <li class="subnav__item">
-                <div class="subnav__group-title">
-                    Ausbau
-                </div>
-                <div class="subnav__item-title">
-                    Interne Wände
-                </div>
+            <li>
+                <a href="#" class="subnav__item">
+                    <div class="subnav__group-title">
+                        Ausbau
+                    </div>
+                    <div class="subnav__item-title">
+                        Interne Wände
+                    </div>
+                </a>
             </li>
 
-            <li class="subnav__item">
-                <div class="subnav__item-title">
-                    Bodenbelag
-                </div>
+            <li>
+                <a href="#" class="subnav__item">
+                    <div class="subnav__item-title">
+                        Bodenbelag
+                    </div>
+                </a>
             </li>
 
-        </ul>
+        </ol>
     </nav>
 </template>
