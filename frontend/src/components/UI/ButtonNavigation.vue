4ea2d6c353af028e024f40e6a0dbfae71b1e71c6
Tim Jungman <tim@humans-machines.com>
2022-02-17 14:12:02 +0100

    Make Big Buttons Components reusable

--- frontend/src/components/UI/ButtonNavigation.vue
+++ frontend/src/components/UI/ButtonNavigation.vue
@@ -1,11 +1,41 @@
 <template>
     <nav class="cluster cluster--stretched pt-300">
-        <a href="/configurator" class="btn btn--xl">
-            <span>Compute</span>
+        <a :href="hrefL" class="btn btn--xl">
+            <span>{{ buttonL }}</span>
         </a>
-        <a href="/learn" class="btn btn--xl">
-            <span>Learn</span>
+        <a :href="hrefR" class="btn btn--xl">
+            <span>{{ buttonR }}</span>
         </a>
     </nav>
 </template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+    props: [
+        'button-l',
+        'button-r',
+        'href-l',
+        'href-r'
+    ],
+    setup(props){
+        const buttonL = computed(()=>{
+            return props['button-l']
+        });
+
+        const buttonR = computed(()=>{
+            return props['button-r']
+        });
+
+        const hrefL = computed(()=>{
+            return props['href-l']
+        });
+
+        const hrefR = computed(()=>{
+            return props['href-r']
+        });
+    }
+}
+</script>
 
