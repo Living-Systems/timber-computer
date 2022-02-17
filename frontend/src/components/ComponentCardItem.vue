58ce786f4f31d8fad3634cbbe069c844626951b7
Adrian Bahr <adrian@humans-machines.com>
2022-02-17 12:07:17 +0100

    Update templates to match new api naming [deploy-kepler]

--- frontend/src/components/ComponentCardItem.vue
+++ frontend/src/components/ComponentCardItem.vue
@@ -2,13 +2,13 @@
     <div class="building-components__comp-row | p-body is-chosen">
             <div>
                 <img src="../../public/assets/dummy-component.png" alt="">
-                <p>{{ element.attributes.Name }}</p>
+                <p>{{ element.attributes.name }}</p>
             </div>
             <!-- <p class="border-2 border-solid rounded-full border-current">A+</p> -->
             <p>A+</p>
-            <p>{{ element.attributes.CradleToSite }}</p>
-            <p>{{ element.attributes.CradleToLife }}</p>
-            <p>{{ element.attributes.CradleToCradle }}</p>
+            <p>{{ element.attributes.cradleToSite }}</p>
+            <p>{{ element.attributes.cradleToLife }}</p>
+            <p>{{ element.attributes.cradleToCradle }}</p>
     </div>
 </template>
 
