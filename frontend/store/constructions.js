cfa35c2ebc7d8aff96a1a036c5249f0e303324ba
Adrian Bahr <adrian@humans-machines.com>
2022-02-17 17:02:29 +0100

    Create constructions.js

---
+++ frontend/store/constructions.js
@@ -0,0 +1,66 @@
+import { atom } from 'nanostores';
+
+const API_URL      = import.meta.env.PUBLIC_STRAPI_URL;
+const API_TOKEN    = import.meta.env.PUBLIC_STRAPI_TOKEN;
+const buildingType = "Typenhaus"
+
+// fetchAPI request to strapi graphql layer
+async function fetchAPI(query, { variables } = {}) {
+	const headers = { 'Content-Type': 'application/json', 'Authorization': API_TOKEN };
+	const res = await fetch(API_URL, {
+		method: 'POST',
+		headers,
+		body: JSON.stringify({ query, variables }),
+	});
+	const json = await res.json();
+	if (json.errors) {
+		console.log(json.errors);
+		throw new Error('Failed to fetch API');
+	}
+	return json.data;
+}
+
+// get a single building by its name
+export async function getBuildingByName(name) {
+	const data = await fetchAPI(`
+        {
+            buildings(filters: { name: { eq: "${name}" } } ) {
+                data {
+                    attributes {
+                        constructions {
+                            data {
+                                attributes {
+                                    name
+                                    components {
+                                        data {
+                                            attributes {
+                                                name
+                                                area
+                                                element {
+                                                    data {
+                                                        attributes {
+                                                            frontendName
+                                                            cradleToSite
+                                                            cradleToLife
+                                                            cradleToCradle
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `);
+	return data?.buildings;
+}
+
+const fetchBuilding = await getBuildingByName(buildingType);
+const constructions = atom(fetchBuilding.data[0].attributes.constructions.data);
+
+export { constructions };
