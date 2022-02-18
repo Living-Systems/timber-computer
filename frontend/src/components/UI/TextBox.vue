8ce71fde8c498048a99585975be56842c0a39934
Tim Jungman <tim@humans-machines.com>
2022-02-18 10:35:27 +0100

    Add compute Page before Configurator

--- frontend/src/components/UI/TextBox.vue
+++ frontend/src/components/UI/TextBox.vue
@@ -1,10 +1,20 @@
 <template>
     <section class="flow-200">
         <h1 class="p-box rounded-sm smoked-glass self-start">
-            Welcome
+            {{ heading }}
         </h1>
         <p class="p-box rounded-md smoked-glass text-style-600">
-            Timber Computer helps you understand the potential carbon impact of a future design by configuring a reference building.
+            {{ content }}
         </p>
     </section>
 </template>
+
+<script setup>
+import {ref} from 'vue';
+
+const props = defineProps(['heading', 'content']);
+
+const heading = ref(props.heading);
+const content = ref(props.content);
+
+</script>
