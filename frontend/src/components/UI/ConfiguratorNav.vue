526b7c2c52abcb1927bb60a54e62e56d75d98fa0
Tim Jungman <tim@humans-machines.com>
2022-02-18 17:04:30 +0100

    Calculate widths for Configurator Subnav based on incoming data

--- frontend/src/components/UI/ConfiguratorNav.vue
+++ frontend/src/components/UI/ConfiguratorNav.vue
@@ -1,21 +1,28 @@
 <template>
     <nav class="subnav">
 
-        <ol class="cluster cluster--stretched items-start">
+        <ol class="cluster items-start" :style="'--total-items: ' + componentCounter">
 
             <!-- first loop: constructions -->
-            <li v-for="construction in constructions" :key="construction.id">
-
+            <li
+                class="subnav__construction-item"
+                :style="'--group-items: ' + construction.counter"
+                v-for="construction in enhancedConstructions"
+                :key="construction.id"
+            >
                 <ol class="cluster cluster--stretched items-start">
 
                     <!-- second loop: components -->
-                    <li v-for="component in construction.attributes.components.data" :key="component">
+                    <li
+                        v-for="component in construction.components"
+                        :key="component"
+                    >
                         <a href="#" class="subnav__item is-set">
                             <div class="subnav__item-title">
                                 {{ component.attributes.name }}
                             </div>
                             <div class="subnav__group-title">
-                                {{ construction.attributes.name }}
+                                {{ construction.name }}
                             </div>
                         </a>
                     </li>
@@ -30,11 +37,37 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 
-import {ref} from 'vue';
-
-const props = defineProps(['building']);
+const props = defineProps(["building"]);
 
 const constructions = ref(props.building.constructions.data);
 
+// calculation for styling
+// componentCounter / enhancedConstructions.counter to calculate width
+
+let componentCounter = 0;
+let enhancedConstructions = [];
+
+for (const construction of constructions.value) {
+    for (const component of construction.attributes.components.data) {
+        componentCounter++;
+
+        let found = enhancedConstructions.find((element) => {
+            return element.construction === construction.attributes.name;
+        });
+
+        if (found) {
+            found.counter++;
+        } else {
+            enhancedConstructions.push({
+                name: construction.attributes.name,
+                counter: 1,
+                components: [component],
+            });
+        }
+    }
+}
+
+console.log("enhancedConstructions", enhancedConstructions);
 </script>
