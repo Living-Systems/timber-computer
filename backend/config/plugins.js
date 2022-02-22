e9f9531f03b746cee92d5627d6da6c8dae84fea6
Adrian Bahr <adrian@humans-machines.com>
2022-02-22 09:59:00 +0100

    Add custom image breakpoints to strapi.

---
+++ backend/config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+    upload: {
+        config: {
+            breakpoints: {
+                xlarge: 1920,
+                large: 1000,
+                medium: 750,
+                small: 500,
+                xsmall: 64
+            },
+        },
+    },
+});
+   
