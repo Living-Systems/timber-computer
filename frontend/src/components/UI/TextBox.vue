2c7489de06596b2eb4497c92472297849ed8ec90
Tim Jungman <tim@humans-machines.com>
2022-02-24 14:45:11 +0100

    Start comparison calculation

--- frontend/src/components/UI/TextBox.vue
+++ frontend/src/components/UI/TextBox.vue
@@ -3,19 +3,25 @@
         <h1 class="p-box rounded-sm smoked-glass self-start">
             {{ heading }}
         </h1>
-        <p class="p-box rounded-md smoked-glass text-style-600">
-            {{ content }}
-            {{ result }}
-        </p>
+        <div class="p-box rounded-md smoked-glass text-style-600">
+            <p v-if="content">{{ content }}</p>
+            <p v-for="value of compareBuilding" :key="value">
+                {{ standardBuilding / value.value }}
+            </p>
+        </div>
     </section>
 </template>
 
 <script setup>
 import {ref} from 'vue';
+import { useStore } from "@nanostores/vue";
+import { standardC02, compareValues } from "../../../store/constructions";
 
 const props = defineProps(['heading', 'content', 'result']);
-
 const heading = ref(props.heading);
 const content = ref(props.content);
+
+const standardBuilding = useStore(standardC02);
+const compareBuilding = useStore(compareValues);
 
 </script>
