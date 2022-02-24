23eb747908505d3bb2f9467284ab74020bfb89e0
Raffael Stüken <r@humans.am>
2022-02-24 15:07:31 +0100

    Fix pages subnav

--- frontend/src/components/UI/LearnNav.vue
+++ frontend/src/components/UI/LearnNav.vue
@@ -1,18 +1,14 @@
 <template>
-    <nav class="subnav | text-style-200 uppercase">
-
+    <nav class="subnav subnav--pages | text-style-200 uppercase">
         <ol class="cluster cluster--stretched items-start">
-
             <li v-for="page in pages" :key="page.slug">
-                <a :href="'/learn/' + page.attributes.slug" class="subnav__item is-set">
-                    <div class="subnav__group-title">
+                <a :href="'/learn/' + page.attributes.slug" class="subnav__item | btn uppercase is-set">
+                    <div class="subnav__item-title">
                         {{ page.attributes.title }}
                     </div>
                 </a>
             </li>
-
         </ol>
-
     </nav>
 </template>
 
