184c3a0b73b591ae7f9ac0d9b25b51040b7b879d
Raffael Stüken <r@humans.am>
2022-02-24 22:56:33 +0100

    Refactor badges

--- frontend/src/components/Computer/ComputerComponentItem.vue
+++ frontend/src/components/Computer/ComputerComponentItem.vue
@@ -9,12 +9,12 @@
         />
 
         <div class="computer-component__item-content | flex p-item">
-            <div class="computer-component__item-head | cluster cluster--y-gap cluster--y-300">
-                <p class="w-full order-last">
-                    {{ element.attributes.frontendName }}
-                </p>
+            <div class="computer-component__item-head | flow-300">
                 <img src="../../../public/assets/dummy-component.png" alt="" />
-                <i :class="sustainabilityClass">{{ element.attributes.sustainability }}</i>
+                <div class="text-center truncate">
+                    <span :class="'badge badge--' + [sustainabilityClass]"><!--{{ element.attributes.sustainability }}--></span>
+                    {{ element.attributes.frontendName }}
+                </div>
             </div>
 
             <div class="computer-component__item-info | cluster cluster--stretched grow text-center" aria-hidden="true">
