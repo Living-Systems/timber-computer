e4993f00c8edffb10ebcd17fe0dd108d40368fb9
Tim Jungman <tim@humans-machines.com>
2022-02-25 11:17:40 +0100

    Structure Learn Module and add image caption

--- frontend/src/components/UI/ContentModules.vue
+++ frontend/src/components/UI/ContentModules.vue
@@ -1,18 +1,32 @@
 <template>
     <div v-for="item in content" :key="item">
-        <div v-if="item.markdown" v-html="micromark(item.markdown)">
-        </div>
+        <div v-if="item.markdown" v-html="micromark(item.markdown)"></div>
+
         <div v-if="item.image">
-            <a v-if="item.url" :href="item.url">
-                <img :src="root + item.image.data.attributes.url" alt="">
-            </a>
-            <img v-else :src="root + item.image.data.attributes.url" alt="">
+            <figure>
+                <a v-if="item.url" :href="item.url">
+                    <img
+                        :src="root + item.image.data.attributes.url"
+                        :alt="item.image.data.attributes.alternativeText"
+                    >
+                </a>
+
+                <img
+                    v-else
+                    :src="root + item.image.data.attributes.url"
+                    :alt="item.image.data.attributes.alternativeText"
+                >
+                
+                <figcaption>{{ item.image.data.attributes.caption }}</figcaption>
+            </figure>
         </div>
     </div>
 </template>
 
 <script setup>
     import {micromark} from 'micromark';
+
+    console.log(props.content);
 
     const root  = import.meta.env.PUBLIC_SERVER_URL;
     const props = defineProps(['content']);
