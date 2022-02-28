55316fb3ffec578457c6fb8ca987d2512cafb54d
Raffael Stüken <r@humans.am>
2022-02-28 11:50:23 +0100

    Refactor learn link to link deep already

--- frontend/src/components/Learn/LearnNav.vue
+++ frontend/src/components/Learn/LearnNav.vue
@@ -2,7 +2,7 @@
     <nav class="subnav subnav--pages | text-style-200 uppercase">
         <ol class="cluster cluster--stretched items-start">
             <li v-for="page in pages" :key="page.slug">
-                <a :href="'/learn/' + page.attributes.slug" class="subnav__item | btn uppercase is-set">
+                <a :href="'/learn/background/' + page.attributes.slug" class="subnav__item | btn uppercase is-set">
                     <div class="subnav__item-title">
                         {{ page.attributes.title }}
                     </div>
