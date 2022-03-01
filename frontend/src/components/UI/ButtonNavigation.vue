69dc52638cc19479aec5bf42bb818de194e36665
Tim Jungman <tim@humans-machines.com>
2022-03-01 16:33:32 +0100

    Restructure button style prop

--- frontend/src/components/UI/ButtonNavigation.vue
+++ frontend/src/components/UI/ButtonNavigation.vue
@@ -2,7 +2,7 @@
     <nav class="cluster cluster--y-gap | pt-300">
         <a v-for="page in pages"
            :key="page.attributes.slug"
-           :class="'btn btn--xl btn--' + style + ' | w-full lg:w-1/2'"
+           :class="buttonStyle + ' btn btn--xl | w-full lg:w-1/2'"
            :href="learn ? '/learn/' + page.attributes.slug : page.attributes.slug">
             <span v-if="page.label" class="whitespace-pre-line text-center">
                 {{ page.label + ': \n' + page.attributes.title}}
@@ -17,7 +17,7 @@
 
 <script setup>
 
-const props = defineProps(['pages', 'style', 'computer', 'learn']);
+const props = defineProps(['pages', 'buttonStyle', 'computer', 'learn']);
 
 </script>
 
